3c26a6a43c79685d7991c98695d9a3b7af7a3397
Каролиночка <karolinocka@MacBook-Mihail.local>
2023-02-19 01:27:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let img1 = document.getElementById('img1')
+
+function getPicture() {
+    img1.src = 'https://www.pravmir.ru/wp-content/uploads/2022/01/two-cats-better-than-one-4-60a762e6cbd43__700.jpg'
+}
+
+function get2Picture() {
+    img1.src = 'https://static.probusiness.io/n/03/d/38097027_439276526579800_2735888197547458560_n.jpg'
+}
